00c9e97360b41209af1ea355f5b63ff06ce4cf09
sumply <sumblychannel@gmail.com>
2024-11-21 00:58:29 +0300

    Добавлена обработка дубликатов

--- Server/DB/database_manager.py
+++ Server/DB/database_manager.py
@@ -1,24 +1,29 @@
 from pymongo import MongoClient
 
-'''
-Для создания объекта нужно передать аргументы:
-    ip и port базы данных,
-    название базы данных
-
-Класс создает две коллекции для хранения тайлов изображений
-и данных юзеров
-'''
-
 
 class Database:
+    '''
+    Класс для работы с mongoDB.
+
+    Атрибуты:
+    host (str): URI подключение.
+    name_db (str): имя базы данных.
+
+
+    Методы:
+    insert_db_image(): создание объектов в коллекцию images.
+    insert_db_user(): создание объектов в коллекцию users.
+    '''
+
     def __init__(self, host: str, name_db: str) -> None:
         self._client = MongoClient(host)
         self._db = self._client[name_db]
 
-    def insert_db_image(self, data) -> None:
+    def insert_db_image(self, data: list[dict]) -> None:
         '''
-        Метод сохраняет в коллекцию image тайлы
-        Пример входного json:
+            Метод сохраняет в коллекцию images тайлы.
+
+            Пример входного json(data):
             [
             {
                 "name" : ...,
@@ -29,34 +34,47 @@ class Database:
             { ... },
             ...
             ]
+            :param data Список словарей тайлов картинок.
         '''
-        self._db["image"].insert_many([
-            {
-                "name": tile["name"],
-                "binary": tile["binary"],
-                "x": tile["x"],
-                "y": tile["y"]
-                } for tile in data
-            ])
+        if not isinstance(data, list):
+            raise ValueError(f"Ожидание list, получен {type(data)}")
+        for tiles in data:
+            if self._db["images"].find_one({"name": tiles["name"]}):
+                raise KeyError("Такая картинка уже есть")
+            self._db["images"].insert_one(
+                    {
+                        "name": tiles["name"],
+                        "binary": tiles["binary"],
+                        "x": tiles["x"],
+                        "y": tiles["y"]
+                    }
+                )
 
-    def insert_db_user(self, data) -> None:
+    def insert_db_user(self, data: list[dict]) -> None:
         '''
-            Метод сохраняет в коллекцию image тайлы
-            Пример входного json:
-                [
-                {
-                    "name" : ...,
-                    "password" : ...,
-                    "roots" : [ ... ]
-                },
-                { ... },
-                ...
-                ]
-        '''
-        self._db["users"].insert_many([
+            Метод сохраняет в коллекцию users информацию
+            о пользователях.
+
+            Пример входного json(data):
+            [
             {
-                "_id": user["name"],
-                "password": user["password"],
-                "roots": user["roots"]
-                } for user in data
-            ])
+                "login" : ...,
+                "password" : ...,
+                "roots" : [ ... ]
+            },
+            { ... },
+            ...
+            ]
+            :param data: Список словарей с данными нового пользователя.
+        '''
+        if not isinstance(data, list):
+            raise ValueError(f"Ожидание list, получен {type(data)}")
+        for user in data:
+            if self._db["users"].find_one({"login": user["login"]}):
+                raise KeyError("Такой пользователь есть")
+            self._db["users"].insert_one(
+                    {
+                        "login": user["login"],
+                        "password": user["password"],
+                        "roots": user["roots"]
+                    })
