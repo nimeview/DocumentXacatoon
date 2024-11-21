995fc4d768006562ec3afe791d915b1a420d8c07
Arthur730 <134970703+Arthur730@users.noreply.github.com>
2024-11-21 20:56:39 +0300

    API 0.1.2
    Оптимизация

--- Server/API/src/upload.rs
+++ Server/API/src/upload.rs
@@ -1,17 +1,16 @@
 use actix_multipart::Multipart;
 use actix_web::{HttpResponse, Responder};
 use futures_util::StreamExt;
-use image::GenericImageView;
+use image::{ImageDecoder};
 use std::io::{Cursor};
-use crate::connection_server::send_tcp;
 
 // Структура для представления тайла
 #[derive(Debug, serde::Serialize)]
 pub struct Tile {
-    pub name: String,
-    pub binary: Vec<u8>,
-    pub x: u32,
-    pub y: u32,
+    name: String,
+    binary: Vec<u8>,
+    x: u32,
+    y: u32,
 }
 
 // Главная асинхронная функция для обработки изображения
@@ -35,9 +34,9 @@ pub async fn process_image(mut payload: Multipart) -> impl Responder {
                     while let Some(Ok(chunk)) = field.next().await {
                         image_data.extend_from_slice(&chunk);
                     }
-
                     // Обработка изображения
-                    if let Err(e) = process_large_image(&image_data, 1024, 1024, &image_name).await {
+                    if let Err(e) = process_large_image(&image_data, 1024, 1024, &image_name).await
+                    {
                         return HttpResponse::InternalServerError()
                             .body(format!("Failed to process image: {}", e));
                     }
@@ -56,17 +55,12 @@ async fn process_large_image(
     tile_width: u32,
     tile_height: u32,
     image_name: &str
-) -> Result<(), Box<dyn std::error::Error>> {
+) -> Result<(), Box<dyn std::error::Error>>
+{
     // Декодируем изображение из памяти
-    let render = image::load_from_memory(image_data)?;
-
-    // Получаем размеры изображения
-    let (width, height) = render.dimensions();
-    let mut buffer = Vec::new();
-
-    // Преобразуем изображение в формат RGB
-    let rgba_image = render.to_rgb8();
-    buffer.extend_from_slice(&rgba_image);
+    let mut reader = image::codecs::png::PngDecoder::new(Cursor::new(image_data))?;
+    let (width, height) = reader.dimensions();
+    let mut buffer = vec![0u8; reader.total_bytes() as usize];
 
     // Разбиваем изображение на тайлы по вертикали и горизонтали
     for y in (0..height).step_by(tile_height as usize) {
@@ -74,8 +68,8 @@ async fn process_large_image(
             let mut tile_buffer = Vec::new();
             // Для каждой строки изображения, относящейся к текущему тайлу, копируем данные в буфер
             for row in y..(y + tile_height).min(height) {
-                let start = (row * width + x) as usize * 3;
-                let end = ((row * width + x + tile_width.min(width - x)) as usize) * 3;
+                let start = (row * width + x) as usize * 4;
+                let end = ((row * width + x + tile_width.min(width - x)) as usize) * 4;
                 tile_buffer.extend_from_slice(&buffer[start..end]);
             }
 
@@ -106,6 +100,6 @@ async fn process_large_image(
 
 // Асинхронная функция для отправки тайла через TCP
 async fn send_tile(tile: Tile) -> Result<(), Box<dyn std::error::Error>> {
-    send_tcp(tile).await?;
+    println!("Sending tile: {}  (x = {}, y = {})", tile.name, tile.x, tile.y);
     Ok(())
 }
